41c95010800dc47cb7b7684a74f8e356b7144d1a
AllociousFranklin <r.allociousfranklin@gmail.com>
2026-03-28 20:46:47 +0530

    production ready

--- src/imports/BrandWordmark.jsx
+++ src/imports/BrandWordmark.jsx
@@ -2,7 +2,7 @@ export default function BrandWordmark({ className = '' }) {
   return (
     <div className={`flex items-center ${className}`}>
       <img 
-        src="/src/assets/brand/logo.svg" 
+        src="/logo.svg" 
         className="h-14 md:h-22 w-auto object-contain" 
         alt="Trielement Studio" 
       />
